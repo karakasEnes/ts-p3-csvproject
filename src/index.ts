cdd8636995bd8f3fb2cda87da3855ab1ff12dae7
Enes Karakaş <enes.karakas.e@gamil.com>
2022-02-01 09:15:53 +0300

    csvfilereader with as  matchResults at row5

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResults } from './MatchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
-enum matchResults {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
-
 let manUnitedWins = 0;
 
 for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === matchResults.HomeWin) {
+  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
     manUnitedWins++;
   }
 
-  if (match[2] === 'Man United' && match[5] === matchResults.AwayWin) {
+  if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
     manUnitedWins++;
   }
 }
