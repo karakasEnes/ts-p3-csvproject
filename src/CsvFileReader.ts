f7cbc9d4b28483950ae56565cd0215e769112ad7
Enes Karakaş <enes.karakas.e@gamil.com>
2022-02-01 14:31:10 +0300

    matchData for each row data type as tuple

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -2,8 +2,10 @@ import fs from 'fs';
 import { MatchResults } from './MatchResult';
 import { stringDateToDate } from './utils';
 
+type MatchData = [Date, string, string, number, number, MatchResults, string];
+
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
@@ -13,15 +15,17 @@ export class CsvFileReader {
     const dataMathes = mathes
       .split('\n')
       .map((row: string): string[] => row.split(','))
-      .map((row: string[]): any => [
-        stringDateToDate(row[0]),
-        row[1],
-        row[2],
-        parseInt(row[3]),
-        parseInt(row[4]),
-        row[5] as MatchResults,
-        row[6],
-      ]);
+      .map(
+        (row: string[]): MatchData => [
+          stringDateToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResults,
+          row[6],
+        ]
+      );
 
     this.data = dataMathes;
   }
