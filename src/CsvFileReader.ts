8c23577844e9e7f4341fa52938d0aeba17bf3e65
Enes Karakaş <enes.karakas.e@gamil.com>
2022-02-03 09:35:34 +0300

    alernate refacotr, MathcReader load functionality, matches property etc

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,7 @@
 import fs from 'fs';
-import { MatchResults } from './MatchResult';
-import { stringDateToDate } from './utils';
-
-type MatchData = [Date, string, string, number, number, MatchResults, string];
 
 export class CsvFileReader {
-  data: MatchData[] = [];
+  data: string[][] = [];
 
   constructor(public filename: string) {}
 
@@ -14,21 +10,8 @@ export class CsvFileReader {
 
     const dataMathes = mathes
       .split('\n')
-      .map((row: string): string[] => row.split(','))
-      .map(this.mapRow);
+      .map((row: string): string[] => row.split(','));
 
     this.data = dataMathes;
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      stringDateToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResults,
-      row[6],
-    ];
   }
 }
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,9 +1,23 @@
-import { CsvFileReader } from './CsvFileReader';
+import fs from 'fs';
 import { MatchResults } from './MatchResult';
 import { stringDateToDate } from './utils';
-
 type MatchData = [Date, string, string, number, number, MatchResults, string];
-export class MatchReader extends CsvFileReader<MatchData> {
+
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
+
+export class MatchReader {
+  matches: MatchData[] = [];
+
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(this.mapRow);
+  }
+
   mapRow(row: string[]): MatchData {
     return [
       stringDateToDate(row[0]),
@@ -16,3 +30,22 @@ export class MatchReader extends CsvFileReader<MatchData> {
     ];
   }
 }
+
+// import { CsvFileReader } from './CsvFileReader';
+// import { MatchResults } from './MatchResult';
+// import { stringDateToDate } from './utils';
+
+// type MatchData = [Date, string, string, number, number, MatchResults, string];
+// export class MatchReader  {
+//   mapRow(row: string[]): MatchData {
+//     return [
+//       stringDateToDate(row[0]),
+//       row[1],
+//       row[2],
+//       parseInt(row[3]),
+//       parseInt(row[4]),
+//       row[5] as MatchResults,
+//       row[6],
+//     ];
+//   }
+// }
